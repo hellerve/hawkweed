6339c4b5c68325b5d0baa23b2d6544298c3a33d0
hellerve <veit.heller@student.htw-berlin.de>
2016-06-08 19:10:50 +0200

    added tests

---
+++ tests/test_collection.py
@@ -0,0 +1,26 @@
+from dandelion.classes import List, Dict
+from nose.tools import *
+
+def test_get_in():
+    assert_equal(List([Dict({1:1})]).get_in(0, 1), 1)
+
+def test_get_in_not_in():
+    assert_equal(List([Dict({1:1})]).get_in(1, 1), None)
+
+def test_get_in_dflt():
+    assert_equal(List([1]).get_in(1, "dflt"), "dflt")
+
+def test_update_in():
+    assert_equal(List([Dict({1:1})]).update_in(0, 1, to=2),
+                 List([Dict({1:2})]))
+
+def test_get_in_not_in():
+    assert_equal(List([Dict({1:1})]).get_in(1, 1), None)
+
+def test_get_in_dflt():
+    assert_equal(List([1]).get_in(1, dflt="dflt"), "dflt")
+
+def test_get_in_reset_dflt():
+    x = List([1])
+    x.DFLT = "dflt"
+    assert_equal(x.get_in(1), "dflt")
