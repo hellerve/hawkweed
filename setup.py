4fa1952fdbad0f94e144fc64ead71743ab0de058
hellerve <veit.heller@student.htw-berlin.de>
2016-06-09 18:58:01 +0200

    version bump due to pypi

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ with open('README.rst') as readme:
 
 setup(
     name = "hawkweed",
-    version = "0.0.3",
+    version = "0.0.4",
     description = "Extending Python builtin types",
     long_description = long_description,
     author = "Veit Heller",
     author_email = "veit@veitheller.de",
     license = "MIT License",
     url = "https://github.com/hellerve/hawkweed",
+    download_url = 'https://github.com/hellerve/hawkweed/tarball/0.0.4',
     packages = find_packages(),
     include_package_data = True,
 )
