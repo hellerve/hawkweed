0f9692fb655e4d3a162d60ae011b80304c587bab
hellerve <veit@veitheller.de>
2017-02-07 11:32:06 +0100

    refactored setup python

--- setup.py
+++ setup.py
@@ -7,16 +7,19 @@ from setuptools import setup
 with open('README.rst') as readme:
     long_description = readme.read()
 
+with open('VERSION') as v:
+    version = v.read().strip()
+
 setup(
-    name = "hawkweed",
-    version = "0.1.5",
-    description = "Extending Python builtin types",
+    name = 'hawkweed',
+    version = version,
+    description = 'Extending Python builtin types',
     long_description = long_description,
-    author = "Veit Heller",
-    author_email = "veit@veitheller.de",
-    license = "MIT License",
-    url = "https://github.com/hellerve/hawkweed",
-    download_url = 'https://github.com/hellerve/hawkweed/tarball/0.1.5',
+    author = 'Veit Heller',
+    author_email = 'veit@veitheller.de',
+    license = 'MIT License',
+    url = 'https://github.com/hellerve/hawkweed',
+    download_url = 'https://github.com/hellerve/hawkweed/tarball/{}'.format(version),
     packages = find_packages(),
     include_package_data = True,
 )
